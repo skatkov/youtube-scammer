e12feefddbed758ece201479d6a16592cc7eee68
SK <lunaticman@gmail.com>
2013-06-30 09:06:32 +0700

    adding more unit-tests

--- spec/lib/string_spec.rb
+++ spec/lib/string_spec.rb
@@ -1,5 +1,5 @@
 require 'rspec'
-require File.dirname(__FILE__) + '/../lib/string.rb'
+require File.dirname(__FILE__) + '/../../lib/string.rb'
 
 describe "String" do
 
--- spec/scammer_spec.rb
+++ spec/scammer_spec.rb
@@ -1,20 +1,34 @@
 require 'rspec'
 require File.dirname(__FILE__) + '/../scammer.rb'
 
-def run
-  begin
-    yield
-  rescue SystemExit => e
-    e.should be_true
-  end
-end
 
 describe 'Scammer' do
   it 'show help' do
-    run{Scammer.new(["-h"]).should_not raise_error}
+    begin
+      Scammer.new(["-h"]).should_not raise_error
+    rescue SystemExit
+    end
   end
-  it "should not accept only one option" do
-    run {Scammer.new(["error"]).to raise_error}
+
+  it 'should not accept only one option' do
+    begin
+      Scammer.new(["error"])
+    rescue OptionParser::InvalidArgument => e
+      e.should_not be_nil
+    end
+  end
+
+  it 'should accept Strings' do
+    client = Scammer.new(["-y", "test1234567", "-p", "test01"])
+    client.options[:video].should eq("test1234567")
+    client.options[:profile].should eq("test01")
+  end
+
+  it 'should accept Array' do
+    client = Scammer.new(["-y", "test1234567", "test1234569", "-p", "test01", "test02"])
+    pp client.options
+    client.options[:video].should eq(["test1234567", "test1234569"])
+    client.options[:profile].should eq(["test01", "test02"])
   end
 
 end
