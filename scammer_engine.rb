0b80d5d04e9446a97cc7859d41ab356668358b88
Stanislav Katkov <lunaticman@gmail.com>
2014-06-13 03:56:16 +0700

    remove one variable dependency

--- scammer_engine.rb
+++ scammer_engine.rb
@@ -7,24 +7,24 @@ class ScammerEngine
   attr_reader :comments, :stats, :client
 
   def initialize(options)
+    @api_key = options[:api_key]
     @scrap_video = []
     @scrap_video << options[:video][0].find_video_id if !options[:video].nil?
     @scrap_profile ||= options[:profile][0].find_user_id if !options[:profile].nil?
-    @client = connect_youtube(options)
     @comments = CommentStat.new
   end
 
-  def connect_youtube(options)
-    options[:api_key].nil? ? YouTubeIt::Client.new : YouTubeIt::Client.new(:dev_key => options[:api_key])
+  def youtube_client
+    @client ||= @api_key.nil? ? YouTubeIt::Client.new : YouTubeIt::Client.new(:dev_key => @api_key)
   end
 
   def find_popular_videos(profile_id)
-    video = ProfileVideos.new(@client.videos_by(:user => profile_id, :most_viewed => TRUE).videos)
+    video = ProfileVideos.new(youtube_client.videos_by(:user => profile_id, :most_viewed => TRUE).videos)
     video.popular.each{|video| add_video(video)}
   end
 
   def add_video(video_id)
-    @comments.add_comments(@client.comments(video_id))
+    @comments.add_comments(youtube_client.comments(video_id))
   end
 
   def run
